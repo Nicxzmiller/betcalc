a8f9d7fb1937739d77526a5e4fa3732578dd7645
Nicholas Markus | StakelogicLive <nicholas.markus@stakelogiclive.com>
2022-06-29 22:50:44 +0200

    betcalc

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <p>This should be the footer then</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Stake from './images/hero.jpg'
+
+const Hero = () => {
+  return (
+    <>
+        <img className='w-full h-80' src={Stake} alt="hero-section" />
+    </>
+  )
+}
+
+export default Hero
+
